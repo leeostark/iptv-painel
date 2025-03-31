849293cd66d028dbcf9bf8faeca9e55ecfd57da0
leeostark <leeo.stark@gmail.com>
2025-03-31 11:52:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 const users = [
     { username: "admin", password: "1234", validUntil: "2025-12-31" },
